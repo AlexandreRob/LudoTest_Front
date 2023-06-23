1e5590f272809954b7d6a52ee56f3540d9bd1570
UgoDIA <112081131+UgoDIA@users.noreply.github.com>
2023-06-23 13:32:33 +0200

    Update service.test.ts

--- ludotest/src/service.test.ts
+++ ludotest/src/service.test.ts
@@ -1,140 +1,85 @@
 import Api from "./service"
 
 const api = new Api()
+const mockData = {
+  id_jeu: 2,
+  nom_jeu: "It's a wonderful world",
+  date_publication: "2015-04-05",
+  age_min: 3,
+  joueurs_min: 4,
+  joueurs_max: 6,
+  id_editeur: 1,
+};
+const id = "1"
+const baseUrl = 'http://127.0.0.1:8000/api/';
+const endpoint = 'JeuViewset';
 
 describe('test api', () => {
   beforeEach(() => {
     global.fetch = jest.fn().mockResolvedValue({
       ok: true,
-      json: jest.fn().mockResolvedValue(
-        {
-          id_jeu: 2,
-          nom_jeu: "It's a wonderful world",
-          date_publication: "2015-04-05",
-          age_min: 3,
-          joueurs_min: 4,
-          joueurs_max: 6,
-          id_editeur: 1,
-        },
-      ),
+      json: jest.fn().mockResolvedValue(mockData),
     });
   });
 
   it('test get', async () => {
-    const data = await api.get('JeuViewset');
-
+    const data = await api.get(endpoint);
     expect(fetch).toHaveBeenCalledTimes(1);
-    expect(fetch).toHaveBeenCalledWith('http://127.0.0.1:8000/api/JeuViewset/', {
+    expect(fetch).toHaveBeenCalledWith(`${baseUrl}${endpoint}/`, {
       method: 'GET',
       headers: {},
     });
-    expect(data).toEqual(
-      {
-        id_jeu: 2,
-        nom_jeu: "It's a wonderful world",
-        date_publication: "2015-04-05",
-        age_min: 3,
-        joueurs_min: 4,
-        joueurs_max: 6,
-        id_editeur: 1,
-      },
-    );
+    expect(data).toEqual(mockData);
   });
 
-  it('test get', async () => {
-    const id = '1'
-    const data = await api.getSingle('JeuViewset', id);
-
+  it('test getSingle', async () => {
+    const data = await api.getSingle(endpoint, id);
     expect(fetch).toHaveBeenCalledTimes(1);
-    expect(fetch).toHaveBeenCalledWith('http://127.0.0.1:8000/api/JeuViewset/1/', {
+    expect(fetch).toHaveBeenCalledWith(`${baseUrl}${endpoint}/${id}/`, {
       method: 'GET',
       headers: {},
     });
-    expect(data).toEqual(
-      {
-        id_jeu: 2,
-        nom_jeu: "It's a wonderful world",
-        date_publication: "2015-04-05",
-        age_min: 3,
-        joueurs_min: 4,
-        joueurs_max: 6,
-        id_editeur: 1,
-      },
-    );
+    expect(data).toEqual(mockData);
   });
 
   it('test postData', async () => {
-    const data = {
-      id_jeu: 2,
-      nom_jeu: "It's a wonderful world",
-      date_publication: "2015-04-05",
-      age_min: 3,
-      joueurs_min: 4,
-      joueurs_max: 6,
-      id_editeur: 1,
-    };
-
-    const responseData = await api.post('JeuViewset', data);
-
+    const responseData = await api.post(endpoint, mockData);
     expect(fetch).toHaveBeenCalledTimes(1);
-    expect(fetch).toHaveBeenCalledWith('http://127.0.0.1:8000/api/JeuViewset/', {
+    expect(fetch).toHaveBeenCalledWith(`${baseUrl}${endpoint}/`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(data),
+      body: JSON.stringify(mockData),
     });
-    expect(responseData).toEqual(data);
+    expect(responseData).toEqual(mockData);
   });
 
   it('test deleteData', async () => {
-    const id = '1';
-
-    await api.delete('JeuViewset', id);
-
+    await api.delete(endpoint, id);
     expect(fetch).toHaveBeenCalledTimes(1);
-    expect(fetch).toHaveBeenCalledWith('http://127.0.0.1:8000/api/JeuViewset/1/', {
+    expect(fetch).toHaveBeenCalledWith(`${baseUrl}${endpoint}/${id}/`, {
       method: 'DELETE',
     });
   });
 
   it('test updateData', async () => {
-    const updatedData = {
-      id_jeu: 2,
-      nom_jeu: "It's a wonderful sky",
-      date_publication: "2021-06-21",
-      age_min: 5,
-      joueurs_min: 2,
-      joueurs_max: 4,
-      id_editeur: 1,
-    };
-
-    const updatedResponse = {
-      id_jeu: 2,
-      nom_jeu: "It's a wonderful sky",
-      date_publication: "2021-06-21",
-      age_min: 5,
-      joueurs_min: 2,
-      joueurs_max: 4,
-      id_editeur: 1,
-    };
-
     global.fetch = jest.fn().mockResolvedValueOnce({
       ok: true,
-      json: jest.fn().mockResolvedValue(updatedResponse),
+      json: jest.fn().mockResolvedValue(mockData),
     });
 
-    const data = await api.update('JeuViewset', '2', updatedData);
+    const data = await api.update(endpoint, id, mockData);
 
     expect(fetch).toHaveBeenCalledTimes(1);
-    expect(fetch).toHaveBeenCalledWith('http://127.0.0.1:8000/api/JeuViewset/2/', {
+    expect(fetch).toHaveBeenCalledWith(`${baseUrl}${endpoint}/${id}/`, {
       method: 'PUT',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(updatedData),
+      body: JSON.stringify(data),
     });
-    expect(data).toEqual(updatedResponse);
+    expect(data).toEqual(mockData);
   });
 
 });
